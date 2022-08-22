3e7feeb46ca6c50d83f0196d7f00ee66017f5216
liuzhanghao <3443718488@qq.com>
2022-08-22 18:12:23 +0800

    节点拖拽

--- src/components/SidebarNode.vue
+++ src/components/SidebarNode.vue
@@ -1,9 +1,3 @@
-<!--
- * @Author: maxf
- * @Date: 2022-03-25 22:49:29
- * @Description: Home组件Sidebar页面
- * @FilePath: \vue3-netforum\src\components\Sidebar.vue
--->
 <template>
   <div class="sidebar">
     <el-row style="margin-top: 10px">
@@ -34,8 +28,8 @@
       </el-col>
     </el-row>
     <div>
-      <el-tree :data="treeData" ref="treeRef" class="filter-tree" highlight-current accordion
-        @current-change="handleNodeClick" node-key="id" :props="defaultProps">
+      <el-tree :data="treeData" ref="treeRef" class="filter-tree" highlight-current draggable
+        @current-change="handleNodeClick" @node-drop="handleDrop" node-key="id" :props="defaultProps">
         <template #default="{ node, data }">
           <span class="custom-tree-node">
             <div class="content">
@@ -117,309 +111,311 @@
   </div>
 </template>
 
-<script>
-import { ref, computed, reactive, nextTick, onMounted, watch, inject } from 'vue'
+<script setup>
+import { ref, computed, reactive, onMounted, watch, inject } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter, useRoute } from "vue-router";
 import { ElMessage, ElMessageBox } from "element-plus";
 import { getCategorysInfo, addCategorys, updateCategorys, deleteCategorys } from '@/api/category.js'
 import { updateForum, getForumInfo } from '@/api/forum.js'
-export default {
-  setup() {
-    const reload = inject('reload')
-    const store = useStore()
-    const route = useRoute()
-    const router = useRouter()
-    const spacename = computed(() => sessionStorage.getItem('spacename'));
-    const spaceid = computed(() => sessionStorage.getItem('spaceid'));
-    // 对话框
-    const dialogNode = ref(false)
-    // 对话框 编辑
-    const dialogEdit = ref(false)
-    // 节点数据 - 全部
-    const treeData = ref([])
-    const treeRef = ref({})
-    // 节点数据 - 单个
-    const nodeData = ref({})
-    // 父节点ID
-    const parent_id = ref('')
-    // 编辑节点
-    const edit_id = ref('')
-    // 配置选项
-    const defaultProps = {
-      children: 'children',
-      label: 'label',
-    }
-    const leftButton = ref(null)
-    const form = ref({
-      name: ''
-    })
-    const formRef = ref(null);
-    // 表单校验
-    const formRules = reactive({
-      name: [{ required: true, message: '请输入分组名称', trigger: 'blur' }]
-    })
 
-    // 获取节点数据
-    const getNodeList = () => {
-      getCategorysInfo(spaceid.value).then((res) => {
-        treeData.value = res.data
+const reload = inject('reload')
+const store = useStore()
+const route = useRoute()
+const router = useRouter()
+const spacename = computed(() => sessionStorage.getItem('spacename'));
+const spaceid = computed(() => sessionStorage.getItem('spaceid'));
+// 对话框
+const dialogNode = ref(false)
+// 对话框 编辑
+const dialogEdit = ref(false)
+// 节点数据 - 全部
+const treeData = ref([])
+const treeRef = ref({})
+// 节点数据 - 单个
+const nodeData = ref({})
+// 父节点ID
+const parent_id = ref('')
+// 编辑节点
+const edit_id = ref('')
+// 配置选项
+const defaultProps = {
+  children: 'children',
+  label: 'label',
+}
+const leftButton = ref(null)
+const form = ref({
+  name: ''
+})
+const formRef = ref(null);
+// 表单校验
+const formRules = reactive({
+  name: [{ required: true, message: '请输入分组名称', trigger: 'blur' }]
+})
+
+// 获取节点数据
+const getNodeList = () => {
+  getCategorysInfo(spaceid.value).then((res) => {
+    treeData.value = res.data
+  })
+}
+
+onMounted(() => {
+  judgeGetNodeList()
+})
+
+watch(() => route.params.notGetNodeList, () => {
+  judgeGetNodeList()
+})
+
+// 根据参数判断是否调用获取节点数据API
+const judgeGetNodeList = () => {
+  if (route.params && route.params.notGetNodeList) {
+    return
+  } else {
+    getNodeList()
+  }
+}
+
+// 指令事件
+const handleCommand = (value) => {
+  if (value == 'root') {
+    form.value.name = ''
+    parent_id.value = spaceid.value
+    dialogNode.value = true
+  }
+}
+
+// 分组指令
+const handleRoot = (command) => {
+  let tmp = command.split(',')
+  if (tmp[0] === 'add') {
+    parent_id.value = tmp[1]
+    dialogNode.value = true
+  }
+  if (tmp[0] === 'edit') {
+    edit_id.value = tmp[1]
+    form.value.name = tmp[2]
+    dialogEdit.value = true
+  }
+  if (tmp[0] === 'remove') {
+    deleteApi(tmp[1])
+  }
+}
+
+// 新建指令 => +号按钮
+const handleNewInstruction = (value) => {
+  let tmp = value.split(',')
+  if (tmp[0] === 'add') {
+    form.value.name = ''
+    parent_id.value = tmp[1]
+    dialogNode.value = true
+  }
+  if (tmp[0] == 'article') {
+    router.push({
+      path: '/tiny',
+      query: {
+        category: tmp[1],
+        isAdd: "add"
+      }
+    })
+  }
+  if (tmp[0] == 'excel') {
+    router.push({
+      path: '/excel',
+      query: {
+        category: tmp[1],
+        isAdd: "add"
+      }
+    })
+  }
+  if (tmp[0] == 'word') {
+    router.push({
+      path: '/md',
+      query: {
+        category: tmp[1],
+        isAdd: "add"
+      }
+    })
+  }
+  if (tmp[0] == 'mindmap') {
+    router.push({
+      path: '/mindmap',
+      query: {
+        category: tmp[1],
+        isAdd: "add"
+      }
+    })
+  }
+}
+
+// 监听下拉菜单的显示/隐藏
+const showIcon = (hidden) => {
+  if (hidden === true) {
+    leftButton.value.style.display = "block"
+    return
+  }
+  leftButton.value.style.display = ""
+}
+
+// 对话框关闭事件
+const dialogClose = () => {
+  formRef.value.resetFields()
+  dialogNode.value = false
+  dialogEdit.value = false
+}
+
+// 新增分组
+const handleAdd = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return
+    let params = {
+      name: form.value.name,
+      parent_category: parent_id.value
+    }
+    addCategorys(params).then((res) => {
+      ElMessage({
+        message: '新增成功！',
+        type: 'success',
       })
-    }
-
-    onMounted(() => {
-      judgeGetNodeList()
+      dialogClose()
+      getNodeList()
     })
+  })
+}
 
-    watch(() => route.params.notGetNodeList, () => {
-      judgeGetNodeList()
-    })
+// 节点编辑 - 保存
+const handleEdit = () => {
+  formRef.value.validate((valid) => {
+    if (!valid) return
+    judegeGetCategory()
+  })
+}
 
-    // 根据参数判断是否调用获取节点数据API
-    const judgeGetNodeList = () => {
-      if (route.params && route.params.notGetNodeList) {
-        return
-      } else {
-        getNodeList()
-      }
-    }
-
-    // 指令事件
-    const handleCommand = (value) => {
-      if (value == 'root') {
-        form.value.name = ''
-        parent_id.value = spaceid.value
-        dialogNode.value = true
-      }
-    }
-
-    // 分组指令
-    const handleRoot = (command) => {
-      let tmp = command.split(',')
-      if (tmp[0] === 'add') {
-        parent_id.value = tmp[1]
-        dialogNode.value = true
-      }
-      if (tmp[0] === 'edit') {
-        edit_id.value = tmp[1]
-        form.value.name = tmp[2]
-        dialogEdit.value = true
-      }
-      if (tmp[0] === 'remove') {
-        deleteApi(tmp[1])
-      }
-    }
-
-    // 新建指令 => +号按钮
-    const handleNewInstruction = (value) => {
-      let tmp = value.split(',')
-      if (tmp[0] === 'add') {
-        form.value.name = ''
-        parent_id.value = tmp[1]
-        dialogNode.value = true
-      }
-      if (tmp[0] == 'article') {
-        router.push({
-          path: '/tiny',
-          query: {
-            category: tmp[1],
-            isAdd: "add"
-          }
-        })
-      }
-      if (tmp[0] == 'excel') {
-        router.push({
-          path: '/excel',
-          query: {
-            category: tmp[1],
-            isAdd: "add"
-          }
-        })
-      }
-      if (tmp[0] == 'word') {
-        router.push({
-          path: '/md',
-          query: {
-            category: tmp[1],
-            isAdd: "add"
-          }
-        })
-      }
-      if (tmp[0] == 'mindmap') {
-        router.push({
-          path: '/mindmap',
-          query: {
-            category: tmp[1],
-            isAdd: "add"
-          }
-        })
-      }
-    }
-
-    // 监听下拉菜单的显示/隐藏
-    const showIcon = (hidden) => {
-      if (hidden === true) {
-        leftButton.value.style.display = "block"
-        return
-      }
-      leftButton.value.style.display = ""
-    }
-
-    // 对话框关闭事件
-    const dialogClose = () => {
-      formRef.value.resetFields()
-      dialogNode.value = false
-      dialogEdit.value = false
-    }
-
-    // 新增分组
-    const handleAdd = () => {
-      formRef.value.validate((valid) => {
-        if (!valid) return
-        let params = {
-          name: form.value.name,
-          parent_category: parent_id.value
-        }
-        addCategorys(params).then((res) => {
-          ElMessage({
-            message: '新增成功！',
-            type: 'success',
-          })
-          dialogClose()
-          getNodeList()
-        })
-      })
-    }
-
-    // 节点编辑 - 保存
-    const handleEdit = () => {
-      formRef.value.validate((valid) => {
-        if (!valid) return
-        judegeGetCategory()
-      })
-    }
-
-    // 判断节点类型走 不同编辑流程
-    const judegeGetCategory = () => {
-      if (nodeData.value.type === 'l') {
-        getUpdateCategorysApi(edit_id.value, form.value)
-      } else {
-        getForumInfo(nodeData.value.articleId).then(res => {
+// 判断节点类型走 不同编辑流程
+const judegeGetCategory = () => {
+  if (nodeData.value.type === 'l') {
+    getUpdateCategorysApi(edit_id.value, form.value)
+  } else {
+    getForumInfo(nodeData.value.articleId).then(res => {
+      if (res.code === 1000) {
+        let nodeForm = {}
+        nodeForm.type = nodeData.value.type
+        nodeForm.title = form.value.name
+        nodeForm.category = res.data.category
+        nodeForm.tags = res.data.tags
+        nodeForm.body = res.data.body
+        updateForum(nodeData.value.articleId, nodeForm).then(res => {
           if (res.code === 1000) {
-            let nodeForm = {}
-            nodeForm.type = nodeData.value.type
-            nodeForm.title = form.value.name
-            nodeForm.category = res.data.category
-            nodeForm.tags = res.data.tags
-            nodeForm.body = res.data.body
-            updateForum(nodeData.value.articleId, nodeForm).then(res => {
-              if (res.code === 1000) {
-                dialogClose()
-                const title = { name: nodeForm.title }
-                getUpdateCategorysApi(edit_id.value, title)
-                getNodeList()
-
-              }
-              reload()
-            })
-          }
-
-        })
-      }
-    }
-
-    // 调用 更新节点API
-    const getUpdateCategorysApi = (id, name) => {
-      updateCategorys(id, name).then((res) => {
-        ElMessage({
-          message: '编辑成功！',
-          type: 'success',
-        })
-        dialogClose()
-        getNodeList()
-      })
-    }
-
-    // 删除接口
-    const deleteApi = (id) => {
-      // 二次确认删除
-      ElMessageBox.confirm("确定要删除吗？", "提示", {
-        confirmButtonText: 'OK',
-        cancelButtonText: 'Cancel',
-        type: "warning",
-        draggable: true,
-      })
-        .then(() => {
-          deleteCategorys(id).then(res => {
-            ElMessage.success("删除成功");
+            dialogClose()
+            const title = { name: nodeForm.title }
+            getUpdateCategorysApi(edit_id.value, title)
             getNodeList()
-            router.push({ name: 'subbooks', params: { wRefresh: true } })
-          })
+
+          }
+          reload()
         })
-        .catch(() => {
-          ElMessage({
-            type: 'info',
-            message: 'Delete canceled',
-          })
-        });
-    }
-
-    // 节点点击事件
-    const handleNodeClick = (node) => {
-      let nodedata = {
-        'label': node.label,
-        'id': node.id
       }
-      nodeData.value = node
-      store.commit("books/SET_NODE_DATA", node);
-      sessionStorage.setItem('node', nodedata)
-      // 判断节点类型,跳转不同路径 ('a', '文章'),('w', 'Word'), ('e', 'Excel'),('m', '思维导图'), ('f', '流程图'), ('p', 'PPT'),('l', '分组'),
-      switch (node.type) {
-        case "l":
-          router.push({ path: '/subbooks' });
-          break;
-        case 'w':
-          router.push({ name: 'detail', params: { wid: node.articleId } })
-          break;
-        case 'e':
-          router.push({ path: '/excel', query: { eid: node.articleId } })
-          break;
-        case 'a':
-          router.push({ name: 'detail', params: { wid: node.articleId } })
-          break;
-      }
-    }
 
-    return {
-      spacename,
-      spaceid,
-      form,
-      parent_id,
-      edit_id,
-      formRef,
-      formRules,
-      treeData,
-      dialogNode,
-      dialogEdit,
-      treeRef,
-      defaultProps,
-      dialogClose,
-      handleRoot,
-      handleNodeClick,
-      handleCommand,
-      handleAdd,
-      handleNewInstruction,
-      handleEdit,
-      showIcon,
-      leftButton,
-      route,
-      judgeGetNodeList,
-      nodeData,
-      judegeGetCategory,
-      getUpdateCategorysApi,
-      reload
+    })
+  }
+}
+
+// 调用 更新节点API
+const getUpdateCategorysApi = (id, name) => {
+  updateCategorys(id, name).then((res) => {
+    ElMessage({
+      message: '编辑成功！',
+      type: 'success',
+    })
+    dialogClose()
+    getNodeList()
+  })
+}
+
+// 删除接口
+const deleteApi = (id) => {
+  // 二次确认删除
+  ElMessageBox.confirm("确定要删除吗？", "提示", {
+    confirmButtonText: 'OK',
+    cancelButtonText: 'Cancel',
+    type: "warning",
+    draggable: true,
+  })
+    .then(() => {
+      deleteCategorys(id).then(res => {
+        ElMessage.success("删除成功");
+        getNodeList()
+        router.push({ name: 'subbooks', params: { wRefresh: true } })
+      })
+    })
+    .catch(() => {
+      ElMessage({
+        type: 'info',
+        message: 'Delete canceled',
+      })
+    });
+}
+
+// 节点点击事件
+const handleNodeClick = (node) => {
+  let nodedata = {
+    'label': node.label,
+    'id': node.id
+  }
+  nodeData.value = node
+  store.commit("books/SET_NODE_DATA", node);
+  sessionStorage.setItem('node', nodedata)
+  // 判断节点类型,跳转不同路径 ('a', '文章'),('w', 'Word'), ('e', 'Excel'),('m', '思维导图'), ('f', '流程图'), ('p', 'PPT'),('l', '分组'),
+  switch (node.type) {
+    case "l":
+      router.push({ path: '/subbooks' });
+      break;
+    case 'w':
+      router.push({ name: 'detail', params: { wid: node.articleId } })
+      break;
+    case 'e':
+      router.push({ path: '/excel', query: { eid: node.articleId } })
+      break;
+    case 'a':
+      router.push({ name: 'detail', params: { wid: node.articleId } })
+      break;
+  }
+}
+
+// 节点拖拽
+// 拖拽完成时触发的事件  参数依次为：被拖拽节点、结束拖拽时最后进入的节点、被拖拽节点的放置位置（before、after、inner）、event
+// 注意：目标节点是已经移动完之后的节点
+const handleDrop = (draggingNode, dropNode, dropType, ev) => {
+  // 定义一个空数组用于存放需要持久化到数据库的节点
+  var paramData = {};
+  // 当拖拽类型不为inner,说明只是在现有的节点间移动，只需要寻找目标节点的父ID，获取其对象以及所有的子节点,data为目标节点的父节点;
+  // 否则，当拖拽类型为inner,说明拖拽节点成为了目标节点的子节点,只需要获取目标节点对象即可,data为目标节点
+  var data = dropType != "inner" ? dropNode.parent.data : dropNode.data;
+
+  // 被拖拽节点的ID
+  var draggingNodeId = draggingNode.data.id;
+  // 被拖拽节点的name
+  var draggingNodeName = draggingNode.data.label
+  // 目标节点的ID
+  var dropNodeId = dropNode.level == 1 && dropType != "inner" ? Number(spaceid.value) : dropNode.data.id
+  console.log("draggingNodeId", draggingNode.data.label);
+  paramData = {
+    name: draggingNodeName, // 被拖拽节点的name
+    parent_category: dropNodeId // 目标节点的ID
+  };
+  updateCategorys(draggingNodeId, paramData).then((res) => {
+    console.log(res);
+    if (res.code == 1000) {
+      ElMessage.success("更新成功");
+      dialogClose()
+      getNodeList()
+      paramData = {}
+    } else {
+      ElMessage.error("更新失败");
     }
-  },
+  })
+
 }
 </script>
 
